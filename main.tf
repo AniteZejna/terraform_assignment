cf3608adf3cea36e7aa7a45f8b97a53f0bac07d9
AniteZejna <anite.zejna@gmail.com>
2023-03-30 21:12:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terrform-assignment-anita-bucket-${var.environment}"
   tags = {
     Environment = var.environment
   }
 }
 
-
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
